b56555c3fe69c9635f28cdfe9163c87642ab4f97
afanimartin <afani.dev@gmail.com>
2024-06-10 16:45:58 +0200

    Create new apps for different exercises

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Button = ({handleButtonclick, text}) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button onClick={handleButtonclick}>{text}</button>
+  );
+}
+
+// conditional rendering
+// the behaviour of the component now depends
+// on whether any buttons have been clicked
+const ButtonClicksHistory = ({allClicks}) => {
+  
+    if (allClicks.length > 0){
+      return (
+        <p>Button press history: {allClicks.join(",")}</p>
+      )
+    }
+  return (
+    <p>Start by clicking the buttons</p>
   )
 }
 
-export default App
+const TotalButtonClicks = ({total}) => {
+  return (
+    <p>Total clicks: {total}</p>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClicks = () => {
+    // [functional update]. a recommended way to update the state.
+    // incase of multiple simultaneous state updates
+    setLeft((left) => left + 1);
+    setAllClicks(allClicks => allClicks.concat("L"));
+    setTotal((total) => total + 1);
+  };
+
+  const handleRightClicks = () => {
+    const newRight = right + 1; // less recommended coz it requires a new var
+    setRight(newRight);
+    const click = allClicks.concat("R");
+    setAllClicks(click);
+    setTotal((total) => total + 1);
+  };
+
+  return (
+    <div>
+      {left}
+      <Button handleButtonclick={handleLeftClicks} text="Left" />
+      <Button handleButtonclick={handleRightClicks} text="Right" />
+      {right}
+      <br />
+      <ButtonClicksHistory allClicks={allClicks} />
+      <TotalButtonClicks total={total} />
+    </div>
+  );
+};
+
+export default App;
